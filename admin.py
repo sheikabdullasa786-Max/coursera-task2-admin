c27d9e870206c08ce35bb858df3c76786fec9485
sheikabdullasa786-Max <sheikabdullasa786@gmail.com>
2025-12-29 19:18:31 +0530

    Refactor admin.py to enhance model registrations
    Refactored admin registration to include Course, Enrollment, Instructor, and updated display fields for Question and Lesson.

--- admin.py
+++ admin.py
@@ -1,30 +1,55 @@
 from django.contrib import admin
-from .models import Question, Choice, Submission, Lesson
+
+from .models import (
+    Course,
+    Lesson,
+    Question,
+    Choice,
+    Submission,
+    Enrollment,
+    Instructor
+)
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 1
+    extra = 2
 
 
 class QuestionInline(admin.TabularInline):
     model = Question
-    extra = 1
+    extra = 2
 
 
+@admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ('question_text',)
+    list_display = ('question_text', 'course', 'grade')
 
 
-
-
+@admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
-    list_display = ('title',)
+    list_display = ('title', 'course')
 
 
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Submission)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('enrollment', 'score')
+
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'course')
+
+
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 
